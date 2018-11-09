c835f18cd9bb0bc9f811c8c7fb753bfd9e5e6b19
Gabriel Henrique <gabrieldesousa.h@gmail.com>
2018-11-08 23:11:47 -0200

    edit

--- js/main.js
+++ js/main.js
@@ -77,6 +77,8 @@ function setUpdate(id) {
 
     document.getElementById("btnUpdate").style.display = "inline-block";
     document.getElementById("btnAdd").style.display = "none";
+
+    document.getElementById("inputIDUpdate").innerHTML = '<input type="hidden" id="idUpdate" value="'+id+'">';
 }
 
 function resetForm() {
@@ -86,6 +88,20 @@ function resetForm() {
     
     document.getElementById("btnUpdate").style.display = "none";
     document.getElementById("btnAdd").style.display = "inline-block";
+
+    document.getElementById("inputIDUpdate").innerHTML = '';
+}
+
+function updateData() {
+    var id = document.getElementById("idUpdate").value;
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+
+    list[id] = {"desc": desc, "amount": amount, "value": value};
+
+    resetForm();
+    setList(list);
 }
 
 setList(list);
